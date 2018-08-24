928b79b8b587681429589fa9b8b0e2741dd44ea5
Franz Noel Tanglao <franz@tanglao.org>
2018-08-24 09:45:06 -0700

    feat: Add cutSticks problem

---
+++ cutSticks.js
@@ -0,0 +1,11 @@
+/**
+<p>You will be given an array of the lengths of a number of sticks.&nbsp; When a turn begins, you must count the number of sticks you have.&nbsp; Determine the length of a stick with the shortest length and discard any sticks of that length.&nbsp; Remove that length from each of the longer sticks and discard the offcuts.&nbsp; Repeat until there are no sticks left.&nbsp; Return an array where the elements are the numbers of sticks you had at the beginning of each turn.</p>
+For example, consider an array representing the lengths of four sticks: [1, 1, 2, 3].  The shortest sticks are 1 unit long.  Discard them.  Remove 1 unit from the other two sticks and discard the scrap.  Now you have two sticks lengths [1, 2].  Do the same and you'll have one stick of length [1].  Discard it and return an array with the number of sticks you had at the start of each turn: [4, 2, 1].
+*/
+
+// Complete the cutSticks function below.
+function cutSticks(lengths) {
+
+
+}
+
