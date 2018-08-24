0ff3f44497937c56cd7c48ef171d85d577f7b85f
Franz Noel Tanglao <franz@tanglao.org>
2018-08-24 09:45:55 -0700

    style:add description for cutSticks

---
+++ index.js
@@ -0,0 +1,37 @@
+var https = require('https');
+
+function getMovieTitles(substr) {
+
+    https.get('https://jsonmock.hackerrank.com/api/movies/search/?Title='+substr, (res) => {
+        var movieList = [];
+        var pages = 0;
+
+        res.on('data', (d) => {
+          var data = JSON.parse(d);
+          pages = JSON.stringify(data.total_pages);
+        });
+
+        console.log(pages);
+
+        // res.on('data', (d) => {
+        //     // var data = JSON.parse(d);
+        //     // var movies = d.data;
+        //     var data = JSON.parse(d);
+        //     var movies = data.data;
+        //     // console.log(movies);
+
+        //     for (var i=0; i < movies.length; i++ ) {
+        //         var movie = movies[i];
+        //         movieList.push(movie.Title);
+        //     }
+
+        //     movieList.sort();
+
+        //     for (var i=0; i<movieList.length;i++) {
+        //         let movie = movieList[i];
+        //         process.stdout.write(movie + "\n");
+        //     }
+        // });
+    });
+}
+getMovieTitles('spiderman');
